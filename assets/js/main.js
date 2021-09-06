37fe708c83c00f3083cfbe575d5357684c4086ae
Naimur53 <naimurrhman53@gmail.com>
2021-09-06 12:53:18 +0600

    done

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+const input = document.getElementById('input');
+const itemWrapper = document.getElementById('item-wrapper');
+const addItem = () => {
+    const text = input.value;
+    if (text === '') {
+        alert('please input something');
+    }
+    else {
+        addToHtml(text);
+        getCart();
+        addItemtToCart(text)
+
+    }
+    input.value = '';
+}
+//adding to html
+const addToHtml = text => {
+    const li = document.createElement('li');
+    li.innerText = text;
+    itemWrapper.appendChild(li);
+}
+//get cart 
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if (cart) {
+        cartObj = JSON.parse(cart);
+
+    }
+    else {
+        cartObj = {};
+    }
+    return cartObj;
+}
+
+// adding item to localStorage
+const addItemtToCart = name => {
+    const cart = getCart();
+    cart[name] = 1;
+    console.log(cart);
+    const cartSring = JSON.stringify(cart);
+    localStorage.setItem('cart', cartSring);
+}
+
+// load cart 
+const loadCart = () => {
+    const cart = getCart();
+    for (const name in cart) {
+        addToHtml(name);
+    }
+}
+
+loadCart();
